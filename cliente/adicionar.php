a4cff288c2178d4ff48540721bb1cf9838d11e59
Cleberpsanches <jscleberpsanches@gmail.com>
2025-06-17 20:02:08 -0300

    f2

--- cliente/adicionar.php
+++ cliente/adicionar.php
@@ -42,3 +42,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   Telefone: <input type="text" name="telefone"><br>
   <button type="submit">Cadastrar</button>
 </form>
+<p><a href="../index.php"><button>Voltar</button></a></p>
